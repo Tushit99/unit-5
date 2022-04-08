8f2e25c951fd55f8a1e654a802ce7d4717fedb2a
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2022-04-08 13:44:24 +0530

    new

--- day-1/src/index.js
+++ day-1/src/index.js
@@ -1,19 +1,25 @@
+import react from "react";
+import { sum, mult } from "./calc";
+import "./index.css"; 
 
-import { sum , mult } from "./calc"  ;   
+import React from "react" ;
+import ReactDOM from "react-dom" ; 
 
-console.log(sum(1,4)) ;  
+console.log(sum(1, 4));
 
-console.log(mult(2,5)) ; 
+console.log(mult(2, 5));
+
+const h2 = React.createElement(
+  "h2",
+  { className: "redtext" },
+  "Welcome To My WEB"
+);
+const img = React.createElement("img", {
+  src: "https://www.seekpng.com/png/detail/215-2151943_-img-anime-character-peeking.png",
+});
+
+const p = React.createElement("p", { className: "flex" }, h2, img);
+const div = React.createElement("div",{className:"on"},h2,img) ; 
 
 
-document.querySelector("#box").addEventListener("click", fun)
-
-function fun(){  
-    // e.preventdefault   
-    let h2 = document.querySelector("#h") ;
-    // h2.innerHTML ="" ;
-    h2.innerText = "Welcome to Masai" ;
-
-    document.querySelector("#h").append(h2) ;  
-}
-
+ReactDOM.render( div , document.getElementById("root"));
--- day-1/webpack.config.js
+++ day-1/webpack.config.js
@@ -1,14 +1,24 @@
-// // if i have to chose the other path/ file to give result ==> entry 
+// // if I have to chose the other path/ file to give result ==> entry 
 
 const path = require("path")
 
 module.exports = {
-    entry : "./src/index.js" ,
+    entry : "./src/index.js" ,    
    output : {  
         path : path.resolve(".","build"),  // // it chose folder  
         filename: "bundle.js"   // // // it selects file/filename   
     },
-    // // to avoid the warning . 
-    mode : "development" ,
+    // // to avoid the warning .    
+    mode : "development" , 
+    module: {
+        rules: [{ test: /\.css$/, use: ["style-loader","css-loader" ]},
+        // {test: /\.(jpe?g|png|svg)$/, use:[{
+        //     loader: "url-loader",
+        //     option: {
+        //         limit: 5000 
+        //     }
+        // }]}
+],
+      },
 }; 
-
+    
