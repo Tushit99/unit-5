d9fb513f933c35e1e7e7eeb524dc0262b2092228
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2022-05-02 17:01:03 +0530

    exam4

---
+++ exam4/src/Redux/store.js
@@ -0,0 +1,22 @@
+import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { authReducer } from './reducer';
+
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+});
+
+
+
+export const store = createStore(
+    rootReducer,
+    window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+);
+
+// s
+console.log(store.getState())
+
+
+
+
+
